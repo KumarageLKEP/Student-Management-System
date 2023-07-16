2e5019e8a44303a62f6dc68307704e463523d5af
KumarageLKEP <esalapriyanath@gmail.com>
2023-07-16 21:44:12 +0530

    Individual Project EG_2020_4033

---
+++ EG_2020_4033/MainWindowVM.cs
@@ -0,0 +1,109 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EG_2020_4033.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace EG_2020_4033
+{
+    public partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<User> users;
+        [ObservableProperty]
+        public User selectedUser = null;
+
+
+
+        public void CloseMainWindow()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+
+
+
+        [RelayCommand]
+        public void messeag()
+        {
+
+            MessageBox.Show($"{selectedUser.FirstName} GPA must be in the range of 0 and 4.", "_ An Error occurred_");
+        }
+
+        [RelayCommand]
+        public void AddStudent()
+        {
+            var vm = new AddUserVM();
+            vm.title = "ADD USER";
+            AddUserWindow window = new AddUserWindow(vm);
+            window.ShowDialog();
+
+            if (vm.Student.FirstName != null)
+            {
+                users.Add(vm.Student);
+            }
+        }
+
+        [RelayCommand]
+        public void Delete()
+        {
+            if (selectedUser != null)
+            {
+                string name = selectedUser.FirstName;
+                users.Remove(selectedUser);
+                MessageBox.Show($"The student {name} is Deleted successfully from the Database.", "_DELETED_");
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Student before press Delete.", "_ An Error occurred_");
+
+
+            }
+        }
+        [RelayCommand]
+        public void ExecuteEditStudentCommand()
+        {
+            if (selectedUser != null)
+            {
+                var vm = new AddUserVM(selectedUser);
+                vm.title = "EDIT USER";
+                var window = new AddUserWindow(vm);
+
+                window.ShowDialog();
+
+
+                int index = users.IndexOf(selectedUser);
+                users.RemoveAt(index);
+                users.Insert(index, vm.Student);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Student before press Edit.", "_ An Error occurred_");
+            }
+        }
+
+        public MainWindowVM()
+        {
+            users = new ObservableCollection<User>();
+            BitmapImage image1 = new BitmapImage(new Uri("/Model/Images/2.png", UriKind.Relative));
+            users.Add(new User(24, "Esala", "Priyanath", "03/03/1999", image1));
+            BitmapImage image2 = new BitmapImage(new Uri("/Model/Images/3.png", UriKind.Relative));
+            users.Add(new User(23, "Saman", "Kumara", "23/01/2001", image2));
+            BitmapImage image3 = new BitmapImage(new Uri("/Model/Images/4.png", UriKind.Relative));
+            users.Add(new User(26, "Mihiri", "Priyanwada", "17/04/1997", image3));
+            BitmapImage image4 = new BitmapImage(new Uri("/Model/Images/1.png", UriKind.Relative));
+            users.Add(new User(25, "Dinuka", "Udayantha", "21/07/1999", image4));
+
+        }
+    }
+}
